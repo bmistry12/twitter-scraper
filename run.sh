9e8f4f2a799ac7bb57a054e962fb0acc0c74eed5
bhm699 <bhm699@cs.bham.ac.uk>
2019-10-23 01:02:10 +0100

    missed out run.sh

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# export LOG_PATH=~/opt/twitter/log
+export LOG_PATH=./logs
+KEYWORDS=$1
+OUTPUT_FILE=$2
+
+echo '=========================== Check Python Version ==========================='
+python --version
+pip --version
+
+echo '=========================== Make Log Files ==========================='
+mkdir ${LOG_PATH}
+touch "$LOG_PATH/hist_twitter_data.log"
+touch "${LOG_PATH}/json_analysis.log"
+touch "${LOG_PATH}/credentials.log"
+
+echo '=========================== Run Credentials.Py ==========================='
+python credentials.py 2>&1 | tee "${LOG_PATH}/credentials.log"
+
+echo '=========================== Run Historic Data Analysis ==========================='
+python historic_twitter_data.py "${KEYWORDS}" "${OUTPUT_FILE}" 2>&1 | tee "$LOG_PATH/hist_twitter_data.log"
+echo ${KEYWORDS}
+echo ${OUTPUT_FILE}
+JSON_FILE="${OUTPUT_FILE}.json"
+echo $JSON_FILE
+if [ -f ${JSON_FILE} ]; then
+    echo "File exists"
+    echo ${OUTPUT_FILE}
+    python json_analysis.py ${JSON_FILE} 2>&1 "${LOG_PATH}/json_analysis.log"
+fi
+
+set -e
